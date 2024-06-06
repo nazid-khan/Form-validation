c6572c85ff51fd6fb28bd900508a217d629f3829
nazid-khan <nazidk773@gmail.com>
2024-06-06 19:05:17 +0530

    form-validation

---
+++ script.js
@@ -0,0 +1,7 @@
+let uername = document.getElementById("username");
+let password = document.getElementById("password");
+let submit = document.getElementById("submit");
+let userError = document.getElementById("user-error");
+let passwordError = document.getElementById("password-error");
+
+
