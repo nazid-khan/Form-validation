3151ac621ccc20ad2ae93ce5df95479cfa7a03d9
nazid-khan <nazidk773@gmail.com>
2024-06-08 18:12:24 +0530

    modify JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-let uername = document.getElementById("username");
-let password = document.getElementById("password");
-let submit = document.getElementById("submit");
-let userError = document.getElementById("user-error");
-let passwordError = document.getElementById("password-error");
 
 
+function validateForm(){
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+    let flag = 1;
+
+    if(username.value == ""){
+         document.getElementById("userError").innerHTML = "Username is Empty";
+         flag = 0;
+    }else if(username.value.length < 3 ){
+         document.getElementById("userError").innerHTML = "Username requires 3 char";
+         flag = 0;
+    }else{
+         document.getElementById("userError").innerHTML = "";
+         flag = 1;
+    }
+
+    if(password.value == ""){
+         document.getElementById("passwordError").innerHTML = "Password is Empty";
+         flag = 0;
+    }else{
+         document.getElementById("passwordError").innerHTML = "";
+         flag = 1;
+    }
+
+    if(flag){
+         return true;
+    }else{
+         return false;
+    }    
+ }
